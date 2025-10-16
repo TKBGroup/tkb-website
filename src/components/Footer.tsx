0aa52382df6ebe3acbb6a2ad5372a7684e9b6b0a
TKB <info@timesinstalls.com>
2025-10-16 13:51:49 -0400

    testimonials section and make it responsive for small screens

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
             <h3 className="text-lg font-bold text-light tracking-wider">
               TKB GROUP
             </h3>
-            <p className="mt-4 max-w-md mx-auto md:mx-0">
+            <p className="mt-4 max-w-md mx-auto md:mx-0 text-light">
               Your trusted partner for commercial millwork and architectural
               woodwork, bringing an impeccable reputation to every project.
             </p>
@@ -23,7 +23,7 @@ export default function Footer() {
               <li>
                 <Link
                   href="/commercial-millwork"
-                  className="hover:text-light transition"
+                  className="text-light hover:text-light transition"
                 >
                   Commercial
                 </Link>
@@ -31,13 +31,13 @@ export default function Footer() {
               <li>
                 <Link
                   href="/residential-woodwork"
-                  className="hover:text-light transition"
+                  className="text-light hover:text-light transition"
                 >
                   Residential
                 </Link>
               </li>
               <li>
-                <Link href="/portfolio" className="hover:text-light transition">
+                <Link href="/portfolio" className="text-light hover:text-light transition">
                   Portfolio
                 </Link>
               </li>
@@ -46,7 +46,7 @@ export default function Footer() {
 
           <div>
             <h4 className="font-semibold text-light mb-4">Contact</h4>
-            <ul className="space-y-2">
+            <ul className="space-y-2 text-light">
               <li>info@tkbmillwork.com</li>
               <li>+1 (416) 675-6555</li>
               <li>112 Snidercroft Road, Unit 7 Concord, ON L4K 4S8</li>
@@ -54,8 +54,8 @@ export default function Footer() {
           </div>
         </div>
 
-        <div className="mt-16 pt-8 border-t border-lightgrey/10 text-center text-lightgrey/50 text-sm">
-          <p>&copy; {currentYear} TKB Group. All Rights Reserved.</p>
+        <div className="mt-16 pt-8 border-t border-gray-4 text-center text-lightgrey/80 text-sm">
+          <p className="text-gray-4">&copy; {currentYear} TKB Group. All Rights Reserved.</p>
         </div>
       </div>
     </footer>
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -7,56 +7,92 @@ import { Pagination, Autoplay } from "swiper/modules";
 import { motion, Variants } from "framer-motion";
 import { GoArrowRight } from "react-icons/go";
 import { quotes } from "./lib/quotes";
+import { useWindowSize } from "./lib/hooks/useWindowSize";
 import "swiper/css";
 import "swiper/css/pagination";
+import styles from "../styles/component_styles/Testimonials.module.css";
 
 const cards = [
   {
     name: "Mary De Pompa",
     desc: "We had a very positive experience with designing, manufacturing and installation of our kitchen nstallation of our kitchen...",
-  },
+    service: "Residential Woodwork",
+  }, 
   {
     name: "Mary De Pompa",
     desc: "We had a very positive experience with designing, manufacturing and installation of our kitchen nstallation of our kitchen...",
-  },
+    service: "Residential Woodwork",
+  }, 
   {
     name: "Mary De Pompa",
     desc: "We had a very positive experience with designing, manufacturing and installation of our kitchen nstallation of our kitchen...",
-  },
+    service: "Residential Woodwork",
+  }, 
+  {
+    name: "Mary De Pompa",
+    desc: "We had a very positive experience with designing, manufacturing and installation of our kitchen nstallation of our kitchen...",
+    service: "Residential Woodwork",
+  }, 
+  {
+    name: "Mary De Pompa",
+    desc: "We had a very positive experience with designing, manufacturing and installation of our kitchen nstallation of our kitchen...",
+    service: "Residential Woodwork",
+  }, 
+  {
+    name: "Mary De Pompa",
+    desc: "We had a very positive experience with designing, manufacturing and installation of our kitchen nstallation of our kitchen...",
+    service: "Residential Woodwork",
+  }, 
+  {
+    name: "Mary De Pompa",
+    desc: "We had a very positive experience with designing, manufacturing and installation of our kitchen nstallation of our kitchen...",
+    service: "Residential Woodwork",
+  }, 
+  {
+    name: "Mary De Pompa",
+    desc: "We had a very positive experience with designing, manufacturing and installation of our kitchen nstallation of our kitchen...",
+    service: "Residential Woodwork",
+  }, 
 ];
+
 
 type ReviewCardProps = {
   name: string;
   desc: string;
+  index: number;
+  service?: string;
 };
 
 const cardVariants: Variants = {
   hidden: { opacity: 0, y: 50 },
   visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
 };
+   
 
-const ReviewCard: React.FC<ReviewCardProps> = ({ name, desc }) => {
+const ReviewCard: React.FC<ReviewCardProps> = ({ name, desc, index, service }) => {
   return (
     <motion.div
       variants={cardVariants}
       initial="hidden"
       whileInView="visible"
       viewport={{ once: true }}
-      className="bg-light h-auto  p-6 rounded-lg shadow-lg max-w-md mx-auto flex flex-col justify-end xl:justify-between m-12 min-h-[260px] "
+      className={`bg-light h-auto  p-3 lg:p-6 rounded-lg shadow-lg max-w-md mx-0 my-10 md:my-23 flex flex-col justify-end xl:justify-between min-h-[200px] lg:min-h-[280px]`}
     >
       <Image
-        src={quotes[0].src}
-        className={`${quotes[0].styles}  mb-4 absolute top-6 right-6`}
+        src={index % 2 == 0 ? quotes[0].src : quotes[1].src}
+        className={`${quotes[0].styles} absolute top-4 sm:top-4 md:top-13 right-4 sm:right-15 md:right-5`}
         alt="User"
         width={50}
         height={50}
       />
-      <p className="md:text-xs lg:text-xs xl:text-lg mt-2 mb-6 xl:mb-0">
+      
+      <h3 className="text-lg font-bold">{service}</h3>
+      <p className="text-xs xs:text-lg md:text-xs lg:text-base xl:text-lg mt-2 mb-6 xl:mb-0">
         {desc}
       </p>
 
       <div className="mt-4 flex flex-col items-end justify-end">
-        <h3 className="lg:text-xs xl:text-base font-bold">{name}</h3>
+        <h3 className="text-xs sm:text-base lg:text-xs xl:text-base font-bold">{name}</h3>
         <div className="text-gold">★★★★★</div>
       </div>
     </motion.div>
@@ -64,34 +100,42 @@ const ReviewCard: React.FC<ReviewCardProps> = ({ name, desc }) => {
 };
 
 export default function Testimonials(): JSX.Element {
+
+  const { width } = useWindowSize();
+  const isMdScreen = width && width >= 768;
+  const isLgScreen = width && width >= 1024;
+  const isXxsScreen = width && width >= 360;
+
   return (
-    <div className="w-full min-h-[300px] h-full flex flex-col px-10  bg-gray-3">
+    <div className="w-full min-h-[450px] h-full flex flex-col px-10 bg-gray-3 bg-[url('/images/reviews-bg.png')] bg-cover bg-center py-20" style={{backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(/images/reviews-bg.png)`}}>
       {/* <h2 className="text-3xl font-bold mb-8">Testimonials</h2> */}
-      <div className="w-full flex max-w-[1420px] h-full min-h-[300px]  max-h-full justify-center gap-8 xl:gap-0">
-        <div className="lg:min-w-30% xl:min-w-[35%] lg:text-5xl xl:text-6xl font-black text-darkgrey font-chivo flex-start">
-          <h3>
+      <div className="w-full flex flex-col md:flex-row max-w-[1420px] h-full min-h-[300px] mx-auto max-h-full justify-center gap-2 md:gap-8 xl:gap-0">
+        <div className="md:min-w-30% xl:min-w-[35%] text-3xl xxs:text-5xl lg:text-5xl xl:text-6xl font-black text-darkgrey font-chivo flex-start my-5 md:my-25">
+          <h3 className="text-light">
             Read <span className="text-gold">Reviews</span>,
           </h3>
-          <h3>Build with</h3>
+          <h3 className="text-light">Build with</h3>
           <h3 className="text-gold">Confidence</h3>
         </div>
-        <motion.div className="relative h-auto w-32 aspect-square bg-light/60 rounded-full xl:mr-18 flex justify-center items-center hover:scale-110 transition-all cursor-pointer shadow-lg shadow-dark/10 my-auto">
+        <motion.div className="hidden md:flex relative h-auto w-18  md:w-32 aspect-square bg-light/60 rounded-full xl:mr-18  justify-center items-center hover:scale-110 transition-all cursor-pointer shadow-lg shadow-dark/10 my-auto">
           <GoArrowRight className="absolute h-auto w-8 " />
         </motion.div>
+        {/* <span className={` ${isXxsScreen ? styles.swiperContainer : {}} w-full max-w-6xl h-full min-h-[450px] max-h-full`}>  */}
         <Swiper
           modules={[Autoplay]}
           pagination={{ clickable: false }}
           autoplay={{ delay: 3000 }}
-          spaceBetween={40}
-          slidesPerView={2.3}
-          className="w-full max-w-6xl h-full min-h-[300px] max-h-full"
+          spaceBetween={isLgScreen ? 35 : 15} 
+          slidesPerView={isLgScreen ? 2.3 : (isMdScreen  ? 1.3 : (isXxsScreen ? 1.2 : 1))}
+          className={` ${isXxsScreen ? styles.swiperContainer : {}} w-full max-w-6xl h-full min-h-[450px] max-h-full`}
         >
           {cards.map((card, index) => (
-            <SwiperSlide className="min-h-[300px]" key={index}>
-              <ReviewCard name={card.name} desc={card.desc} />
+            <SwiperSlide className="min-h-[450px]" key={index}>
+              <ReviewCard name={card.name} desc={card.desc} index={index} service={card.service}/>
             </SwiperSlide>
           ))}
         </Swiper>
+        {/* </span> */}
       </div>
     </div>
   );
